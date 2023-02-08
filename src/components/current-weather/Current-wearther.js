bdd8a81bbaae6a9b7d47b41d5e4af9c4e5f54cec
singhutsav5502 <singhutsavofficial@gmail.com>
2023-02-08 18:22:01 +0530

    edit css

--- src/components/current-weather/Current-wearther.js
+++ src/components/current-weather/Current-wearther.js
@@ -4,11 +4,29 @@ const CurrentWeather = () => {
     return (
         <div className="weather-container">
             <div className="weather">
-                <div className="top">
-                    <p className="city">Name</p>
-                    <p className="description">Description</p>
+                <div className="left">
+                    <p className="city">Name  </p>
+                    <p className="temperature">  18°C</p>
+                    <div className='description-container'>
+                        <img alt="weather" className="weather-icon" src="icons/01d.png" />
+                        <p className="description">Description</p>
+                    </div>
                 </div>
-                <img alt="weather" className="weather-icon" src="icons/01d.png" />
+                <div className="right">
+                    <div className='right-items'>
+
+                        <div className="details">
+                            <div className="parameter-row">
+                                <span className="parameter-label">Details</span>
+                            </div>
+                            <div className="parameter-row">
+                                <span className="parameter-label">Feels like</span>
+                                <span className="parameter-value"> 22℃</span>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
