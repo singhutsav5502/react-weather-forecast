d02c53bdb75cc44f5d6d66b4dc898be077046756
singhutsav5502 <singhutsavofficial@gmail.com>
2023-02-09 05:45:29 +0530

    edit css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import Search from './components/search/Search';
 import CurrentWeather from './components/current-weather/Current-wearther';
 function App() {
-  const handleOnSearchChange = (searchData) => { console.log(searchData) };
+  const handleOnSearchChange = (searchData) => { 
+    console.log(searchData) 
+  };
   return (
     <div className="container">
       <div className="search">
--- src/components/current-weather/Current-wearther.js
+++ src/components/current-weather/Current-wearther.js
@@ -5,25 +5,33 @@ const CurrentWeather = () => {
         <div className="weather-container">
             <div className="weather">
                 <div className="left">
-                    <p className="city">Name  </p>
                     <p className="temperature">  18°C</p>
                     <div className='description-container'>
                         <img alt="weather" className="weather-icon" src="icons/01d.png" />
                         <p className="description">Description</p>
+                        <p className="city">Name  </p>
                     </div>
                 </div>
                 <div className="right">
                     <div className='right-items'>
 
                         <div className="details">
-                            <div className="parameter-row">
-                                <span className="parameter-label">Details</span>
-                            </div>
-                            <div className="parameter-row">
+                        <div className="parameter-row">
                                 <span className="parameter-label">Feels like</span>
-                                <span className="parameter-value"> 22℃</span>
+                                <span className="parameter-value"> 22°C </span>
                             </div>
-
+                            <div className="parameter-row">
+                                <span className="parameter-label">Wind</span>
+                                <span className="parameter-value"> 2 m/s</span>
+                            </div>
+                            <div className="parameter-row">
+                                <span className="parameter-label">Humidity</span>
+                                <span className="parameter-value"> 15 %</span>
+                            </div>
+                            <div className="parameter-row">
+                                <span className="parameter-label">Pressure</span>
+                                <span className="parameter-value"> 15 hPa </span>
+                            </div>
                         </div>
                     </div>
                 </div>
