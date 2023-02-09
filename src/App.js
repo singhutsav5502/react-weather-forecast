fe20ee1839ee767c4e48fbb9b65d15e3ea40bdb0
singhutsav5502 <singhutsavofficial@gmail.com>
2023-02-09 06:10:09 +0530

    Setup dummy and weather api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React , {useState} from 'react';
 import './App.css';
 import Search from './components/search/Search';
 import CurrentWeather from './components/current-weather/Current-wearther';
+import CurrentWeatherDummy from './components/current-weather/Current-wearther-dummy';
+import { WEATHER_API_KEY,WEATHER_API_URL } from './api';
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
   const handleOnSearchChange = (searchData) => { 
-    console.log(searchData) 
+    const[lat,lon] = searchData.value.split(" ");
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+
+    Promise.all([currentWeatherFetch,forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({city:searchData.label, ...weatherResponse});
+        setForecast({city:searchData.label, ...forecastResponse});
+      })
+      .catch((err)=> {console.log(err)});
   };
+  console.log(currentWeather);
+  console.log(forecast);
   return (
     <div className="container">
       <div className="search">
@@ -13,7 +32,7 @@ function App() {
       </div>
       <div className="weatherSide">
         <h1>Today</h1>
-        <CurrentWeather />
+        {currentWeather?<CurrentWeather data = {currentWeather}/>:<CurrentWeatherDummy/>}
       </div>
     </div>
   );
--- src/components/current-weather/Current-wearther-dummy.js
+++ src/components/current-weather/Current-wearther-dummy.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Current-weather.css'
-const CurrentWeather = ({data}) => {
+const CurrentWeatherDummy = () => {
     return (
         <div className="weather-container">
             <div className="weather">
                 <div className="left">
-                    <p className="temperature">  18°C</p>
+                    <p className="temperature">  Temperature </p>
                     <div className='description-container'>
                         <img alt="weather" className="weather-icon" src="icons/01d.png" />
                         <p className="description">Description</p>
-                        <p className="city">Name  </p>
+                        <p className="city">Select Location  </p>
                     </div>
                 </div>
                 <div className="right">
@@ -18,19 +18,19 @@ const CurrentWeather = ({data}) => {
                         <div className="details">
                         <div className="parameter-row">
                                 <span className="parameter-label">Feels like</span>
-                                <span className="parameter-value"> 22°C </span>
+                                <span className="parameter-value"> - </span>
                             </div>
                             <div className="parameter-row">
                                 <span className="parameter-label">Wind</span>
-                                <span className="parameter-value"> 2 m/s</span>
+                                <span className="parameter-value"> -</span>
                             </div>
                             <div className="parameter-row">
                                 <span className="parameter-label">Humidity</span>
-                                <span className="parameter-value"> 15 %</span>
+                                <span className="parameter-value"> -</span>
                             </div>
                             <div className="parameter-row">
                                 <span className="parameter-label">Pressure</span>
-                                <span className="parameter-value"> 15 hPa </span>
+                                <span className="parameter-value"> - </span>
                             </div>
                         </div>
                     </div>
@@ -39,4 +39,4 @@ const CurrentWeather = ({data}) => {
         </div>
     );
 };
-export default CurrentWeather;
+export default CurrentWeatherDummy;
