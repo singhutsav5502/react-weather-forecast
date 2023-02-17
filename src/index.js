c59c2471cef1d2d54132fa24fd63bb370f84ba0c
singhutsav5502 <singhutsavofficial@gmail.com>
2023-02-17 17:18:21 +0530

    fix whitespace warning

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const themeImageHandler = (isLight) => {
     if (isLight) {
-        document.body.style.backgroundImage = `${`url(${process.env.PUBLIC_URL}/day3.jpg)`}`;
+        document.body.style.backgroundImage =`${`url(${process.env.PUBLIC_URL}/day3.jpg)`}`;
     }
     else {
-        document.body.style. backgroundImage =  `${`url(${process.env.PUBLIC_URL}/spaceBack3.jpg)`}`;
+        document.body.style.backgroundImage =`${`url(${process.env.PUBLIC_URL}/spaceBack3.jpg)`}`;
     }
 }
 root.render(
